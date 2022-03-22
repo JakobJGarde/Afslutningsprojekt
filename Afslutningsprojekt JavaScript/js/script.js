404340f40c2ac4d808591f0e78c1d713924e2344
Jakob Garde <99877872+JakobJGarde@users.noreply.github.com>
2022-03-22 18:15:33 +0100

    Filer

---
+++ Afslutningsprojekt JavaScript/js/script.js
@@ -0,0 +1,9 @@
+// console.log("Test");
+// var varName = value;
+// function functionName() {}
+// for (var i = value; condition, i++ eller i--)
+
+// Når du tildele en variable et HTML-element:
+// var varName = document.getElementById("idName");
+// du refferere til et HTML-elements HTML attribute ved at skrive . efterfulgt af din HTML attribute.
+// Eksempelvis: htmlElement.style.color     eller: html.src
